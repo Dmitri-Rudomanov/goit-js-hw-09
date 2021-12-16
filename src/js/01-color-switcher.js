08f4a9b4325a0587400b4898ed3ed1fa4d1e0a7f
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2021-12-16 10:21:36 +0200

    color-switch-ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+const refs={ 
+    startBtn : document.querySelector("[data-start]"),
+    stopBtn : document.querySelector("[data-stop]"),
+    body:document.querySelector("body")
+}
+let timerId=null
+
+refs.startBtn.addEventListener("click", onStartBtn)
+refs.stopBtn.addEventListener("click", onStopBtn)
+
+
+function onStartBtn() { 
+    timerId=setInterval(() => { 
+        refs.body.style.backgroundColor = getRandomHexColor()
+    },1000)
+}
+function onStopBtn() { 
+    clearInterval(timerId)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
