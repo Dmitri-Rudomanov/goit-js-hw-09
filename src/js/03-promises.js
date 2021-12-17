ade2f0c068e34146b5e94662e3d1fb8943fbe57b
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2021-12-17 11:39:07 +0200

    promise-work

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+
+const refs = {
+  delay:document.querySelector(`input[name="delay"]`),
+  step:document.querySelector(`input[name="step"]`),
+  amount: document.querySelector(`input[name="amount"]`),
+  form:document.querySelector(".form")
+}
+
+refs.form.addEventListener("submit",onFormSubmit)
+let delay = 0
+
+function onFormSubmit(e) {
+  e.preventDefault()
+  for (var i = 1; i <= refs.amount.value; i++) {
+    delay+=parseInt(refs.delay.value)
+    createPromise(i, delay)
+      .then((result) => { console.log(result) })
+      .catch((error) => { console.log(error) })
+}
+
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  
+  return new Promise((resolve, reject) => { 
+    const shouldResolve = Math.random() > 0.3;
+
+  setInterval(() => { 
   if (shouldResolve) {
     // Fulfill
+   resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
+  reject(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+    }, delay)
+  })
 }
